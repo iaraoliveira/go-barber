00d3369f573e2aed6167b9a73bd2f62adfd0ae22
Iara Oliveira <iara99oliveira@gmail.com>
2020-07-25 00:04:35 -0300

    Cria tabela de usuários e adiciona relacionamento entre as tabelas

--- backend/src/database/migrations/1595641307196-CreateUsers.ts
+++ backend/src/database/migrations/1595641307196-CreateUsers.ts
@@ -1,11 +1,11 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import {MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreateAppointments1595549225357 implements MigrationInterface {
+export class CreateUsers1595641307196 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: "appointments",
+                name: "users",
                 columns: [{
                     name: 'id',
                     type: 'uuid',
@@ -14,14 +14,17 @@ export class CreateAppointments1595549225357 implements MigrationInterface {
                     default: 'uuid_generate_v4()'
                 },
                 {
-                    name: 'provider',
+                    name: 'name',
                     type: 'varchar',
-                    isNullable: false,
                 },
                 {
-                    name: 'date',
-                    type: 'timestamp with time zone',
-                    isNullable: false,
+                    name: 'email',
+                    type: 'varchar',
+                    isUnique: true,
+                },
+                {
+                    name: 'password',
+                    type: 'varchar',
                 },
                 {
                     name: 'created_at',
@@ -38,7 +41,7 @@ export class CreateAppointments1595549225357 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments');
+        await queryRunner.dropTable('users');
     }
 
 }
