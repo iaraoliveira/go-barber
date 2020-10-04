f494440f46921ed8e0acb66bf287cc0bbb4d62b0
iaraoliveira <iara99oliveira@gmail.com>
2020-10-04 19:55:41 -0300

    Implementing tests with Jest

---
+++ backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,34 @@
+import { isEqual } from 'date-fns';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO'
+
+import Appointment from '../../infra/typeorm/entities/Appointment';
+import { uuid } from 'uuidv4';
+
+
+class AppointmentsRepository implements IAppointmentsRepository {
+    private appointment: Appointment[] = [];
+
+    public async findByDate(date: Date) : Promise<Appointment | undefined> {
+
+        const findAppointment = this.appointment.find(appointment => {
+            appointment.date === date
+        })
+
+        return findAppointment;
+    }
+
+    public async create({provider_id, date}:ICreateAppointmentDTO) : Promise<Appointment> {
+        const appointment = new Appointment();
+
+        Object.assign(appointment, { id: uuid(), date, provider_id })
+        
+        this.appointment.push(appointment);
+
+        return appointment;
+    }
+
+}
+
+export default AppointmentsRepository;
